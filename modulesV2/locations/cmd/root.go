c555e1390c04bd21357039e17543ec46e39c902c
Ayman <enkhalifapro@gmail.com>
2024-12-03 16:20:12 +0200

    add project docs
    Signed-off-by: Ayman <enkhalifapro@gmail.com>

--- modulesV2/locations/cmd/root.go
+++ modulesV2/locations/cmd/root.go
@@ -11,27 +11,7 @@ var (
 		Short: "locations service manages locations data",
 		Long:  `locations service manages locations data`,
 		Run: func(cmd *cobra.Command, args []string) {
-			// todo: move to connections server then delete me
-			/*topic := "test1"
-			partition := 0
-
-			r := kafka.NewReader(kafka.ReaderConfig{
-				Brokers:   []string{"kafka:9092"},
-				Topic:     topic,
-				Partition: partition,
-			})
-
-			for {
-				m, err := r.ReadMessage(context.Background())
-				if err != nil {
-					log.Fatal("failed to write messages:", err)
-				}
-				// Print the message
-				fmt.Printf("Message received: %s\n", string(m.Value))
-
-				// Simulate processing time
-				time.Sleep(1 * time.Second)
-			}*/
+			
 		},
 	}
 )
